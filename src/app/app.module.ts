f3fcf7895b4011ca3db2c2b23861788e9859c73c
David Zimberknopf <dudi@vizo.dev>
2021-02-22 16:07:22 +0200

    updating bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
@@ -18,7 +17,6 @@ import { httpInterceptorProviders } from './core/api/interceptors';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     DashboardModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
